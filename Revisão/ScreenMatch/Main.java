b1d741687b09a9c04021c1cce1d38d69bc81bc62
jeffersoncdantas <jeffersoncdantas@yahoo.com>
2024-03-02 18:32:40 -0300

    Create Main.java

---
+++ Revisão/ScreenMatch/Main.java
@@ -0,0 +1,26 @@
+package Revisão.ScreenMatch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        
+        double media =  (9.8 + 6.3 + 8.0)/3;
+
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = """
+            Filme de aventura
+            dos anos 80
+            """;
+        System.out.println(sinopse);
+
+    }
+
+}
