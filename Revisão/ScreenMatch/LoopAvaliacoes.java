81ea5b96d2f5c31d6b04d2a6b0b9358e3af3e5ce
jeffersoncdantas <jeffersoncdantas@yahoo.com>
2024-03-02 21:34:24 -0300

    Create LoopAvaliacoes.java

---
+++ Revisão/ScreenMatch/LoopAvaliacoes.java
@@ -0,0 +1,21 @@
+package Revisão.ScreenMatch;
+
+import java.util.Scanner;
+
+public class LoopAvaliacoes {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite a sua avaliacao para o filme: ");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.printf("Media de avaliação: %.2f", (mediaAvaliacao/3));
+        leitura.close();
+    }
+    
+}
